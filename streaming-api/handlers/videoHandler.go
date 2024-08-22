d7e5bc6d9030c2f3c84f561df663ca666f126d4b
mishaanikutin <billyknife@mail.ru>
2024-08-22 13:18:23 +0300

    [4] Первая версия. Открывает видео

--- streaming-api/handlers/videoHandler.go
+++ streaming-api/handlers/videoHandler.go
@@ -11,15 +11,16 @@ import (
 
 func InitRoutes() *gin.Engine {
 	router := gin.New()
+	router.Use(CORSMiddleware())
 
-	video_router := router.Group("/video")
+	video_router := router.Group("/api/video")
 	video_router.GET("/:name", videoHandler)
 
 	return router
 }
 
 func videoHandler(c *gin.Context) {
-	name := c.Param("name") + ".m3u8"
+	name := c.Param("name")
 
 	Video := filepath.Join("..", "static", "videos", name)
 
@@ -31,5 +32,22 @@ func videoHandler(c *gin.Context) {
 
 	} else {
 		c.Status(http.StatusNotFound)
+	}
+}
+
+func CORSMiddleware() gin.HandlerFunc {
+	return func(c *gin.Context) {
+
+		c.Header("Access-Control-Allow-Origin", "*")
+		c.Header("Access-Control-Allow-Credentials", "true")
+		c.Header("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
+		c.Header("Access-Control-Allow-Methods", "POST, HEAD, PATCH, OPTIONS, GET, PUT")
+
+		if c.Request.Method == "OPTIONS" {
+			c.AbortWithStatus(204)
+			return
+		}
+
+		c.Next()
 	}
 }
